db4e0c49a397429764810e85085d6e1339321f19
radlinsky <cmr273@cornell.edu>
2016-02-08 19:07:45 -0500

    update

--- split_at_column.py
+++ split_at_column.py
@@ -13,7 +13,7 @@
 ###      file_delim: how is the file delimited?
 ###          String.
 ###
-###      head: how many lines of stuff before the data begins?
+###      skip: how many lines of stuff to skip?
 ###          Integer.
 ###
 ###      out_file: what should the new file be named?
@@ -30,13 +30,20 @@
 ###      Assumptions:
 ###          Column of interest is identically formatted at every row.
 ###
+###  Examples:
+###    split_me_up | col2 | col3
+###
+###    replace = 'n'
+###    split | me | up | split_me_up | col2 | col3
+###
+###    replace = 'y'
+###    split | me | up | col2 | col3
 ###  Usage:
-###    python split_at_column.py in_file file_delim head out_file col_i col_delim replace
+###    python split_at_column.py in_file file_delim skip out_file col_i col_delim replace
 
 import sys
 import os
 import csv
-from subprocess import call
 
 print "Initiating split_at_column.py"
 print "Argument List:", str(sys.argv[1:])
@@ -46,19 +53,66 @@ if (len(sys.argv)-1 != 7):
 
 in_FILE = str(sys.argv[1])
 file_delim = str(sys.argv[2])
-head = int(sys.argv[3])
-out_file = str(sys.argv[4])
+skip = int(sys.argv[3])
+out_FILE = str(sys.argv[4])
 col_i = int(sys.argv[5])
 col_delim = str(sys.argv[6])
 replace = str(sys.argv[7])
 
 if not (os.path.isfile(in_FILE)):
     raise ValueError(in_FILE+" not found. Is it a *full* and valid file path?")
-if not head >= 0:
-    raise ValueError("head should be integer >= 0")
-if not os.path.isdir(os.path.dirname(out_file)):
-    raise ValueError(out_file+" can't be written because the parent dir doesn't exist.")
+if not skip >= 0:
+    raise ValueError("skip should be integer >= 0")
+if not os.path.isdir(os.path.dirname(out_FILE)):
+    raise ValueError(out_FILE+" can't be written because the parent dir doesn't exist.")
 if col_i < 0:
     raise Exception("col_i needs to be an integer >= 0.")
 if replace != "y" or replace != "n":
     raise ValueError("replace needs to be 'y' or 'n' [lowercase]")
+
+# Initiate new file
+split_file_rows = list()
+with open(in_FILE, 'rb') as file_handle:
+    content = file_handle.readlines()
+    i = 0
+    for line in content:
+        if i < skip:
+            continue
+        if file_delim not in line:
+            raise ValueError("Delim: '"+file_delim+"' doesn't seem to be the delim for the input file...")
+        split_line = line.rstrip('\r\n').split(file_delim)
+        
+        # Pop off column if replace = 'y'
+        if replace == "y":
+            col_to_split = split_line.pop(col_i)
+        # Else keep the column
+        else:
+            col_to_split = split_line[col_i]
+        if col_delim not in col_to_split:
+            raise ValueError("Delim: '"+col_delim+"' doesn't seem to be in the col of interest at line "+str(i))
+        
+        # Split the column at specified delim
+        split_col = col_to_split.split(col_delim)
+        
+        # Add the split column elements to the beginning of the row, preserving order of the split col
+        while len(split_col) > 0:
+            split_line.insert(0, split_col.pop())
+        
+        # Add row with column split up to the new data file
+        split_file_rows.append(split_line)
+        i = i + 1
+
+# Write new file
+with open(out_FILE, 'wb') as file_handle:
+    writer = csv.writer(file_handle)
+    writer.writerows(split_file_rows)
+
+
+
+
+
+
+
+
+
+
