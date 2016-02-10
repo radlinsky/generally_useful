4d4c0c9e99040943d152305919a8c292fd5c8345
radlinsky <cmr273@cornell.edu>
2016-02-10 16:50:37 -0500

    quickie fixie

--- helper_functions.py
+++ helper_functions.py
@@ -99,7 +99,7 @@ def make_scisub_job_command(
 	if type(ErrOut) is not bool:
 		raise ValueError("ErrOut needs to be a boolean.")
 
-	if Script[-2:] != "py" and Script[-2:] != "R":
+	if Script[-2:] != "py" and Script[-1:] != "R":
 		raise ValueError("Expected a .py python or .R R script, instead got: "+Script)
 
 	if not (os.path.isdir(ScriptDir)):
@@ -206,7 +206,7 @@ def make_consign_job_command(
 	if type(ErrOut) is not bool:
 		raise ValueError("ErrOut needs to be a boolean.")
 
-	if Script[-2:] != "py" and Script[-2:] != "R":
+	if Script[-2:] != "py" and Script[-1:] != "R":
 		raise ValueError("Expected a .py python or .R R script, instead got: "+Script)
 
 	if not (os.path.isdir(ScriptDir)):
